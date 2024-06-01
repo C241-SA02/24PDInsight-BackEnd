3b27f610e5c4391c5ea5995700dcdaf9b7abb89f
hanifsetyadi <hfsetyadi21@gmail.com>
2024-06-02 00:58:27 +0700

    merge upload api & add delete file bucket feature

--- src/handler.js
+++ src/handler.js
@@ -5,94 +5,146 @@ const axios = require('axios');
 axios.defaults.baseURL = 'https://m0t98818-5000.asse.devtunnels.ms/';
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-const uploadFileHandler = async(req, res, bucket, bucketName) => {
-    if (!req.file) {
-        console.log("No file uploaded");
-        return res.status(400).json('No file uploaded.');
-    }
-
-    const timestamp = Date.now();
-    const randomNum = Math.floor(Math.random() * 10000);
-    const fileExtension = path.extname(req.file.originalname);
-    const newFileName = `audio-${timestamp}-${randomNum}${fileExtension}`;
-
-    console.log("File Uploaded: ", req.file);
-
-    const bufferStream = new stream.PassThrough();
-    bufferStream.end(req.file.buffer);
-
-    try {
-        const file = bucket.file(newFileName)
-        bufferStream.pipe(file.createWriteStream({
-            resumable: false,
-            metadata: {
-                contentType: req.file.mimetype
-            }
-        }))
-        .on('finish', () => {
-            console.log(`File uploaded to https://storage.googleapis.com/${bucketName}/${newFileName}`);
-            return res.status(200).json('File and form data uploaded and processed.');
+const uploadHandler = async (req, res, bucket, bucketName) => {
+    if (!req.file && !req.body.link) {
+        console.log("No file or link attached");
+        return res.status(400).json({
+            message: "No file or link attached. Please Attach File"
         })
-        .on('error', (err) => {
-            console.error('Error uploading file to Google Cloud Storage:', err);
-            return res.status(500).send('Error uploading file.');
-        });
-    } catch (error) {
-        console.error('Error during file upload process:', error);
-        return res.status(500).send('Error uploading file.');
-    }
-}
-
-const sentimentAnalysisHandler = async (req,res) => {
-    const { text } = req.body;
-    
-    if (!req.body) {
-        console.log("no file uploaded");
-        return res.status(400).json("No text input");
     }
 
-    try {
-        const response = await axios.post('/predict', {
-            text: text
-        })
+    console.log(req.file);
+    console.log(req.body.link);
 
-        console.log(response.data);
-        return res.status(200).json({message: "Succeed Doing Sentiment Analysis", result: `${JSON.stringify(response.data)}`})
-    } catch (error) {
-        console.log(error);
-        return error;
+    // If a link is provided, handle it
+    if (req.body.link) {
+        console.log("ke link ", req.body.link);
+        return linkHandler(res, req.body.link);
     }
-}
 
-const uploadLinkHandler = async(req,res) => {
-    const { link } = req.body;
+    // If a file is provided, handle it
+    if (req.file) {
+        console.log("ke file");
+        return file(req, res, bucketName, bucket);
+    }
+};
 
-    if (!req.body) {
+const linkHandler = async (res, link) => {
+    if (!link) {
         console.log("Body empty");
-        return res.status(400).json({message: "Failed"})
+        return res.status(400).json({ message: "Failed" });
     }
 
-const tempURL = "https://www.youtube.com/watch?v=lilaaEvEEjw";
-
     try {
-        const response = await axios.post('/transcribe', {
-            url: tempURL
-        });
+        const transcribeResult = await axios.post('/transcribe', { url: link });
+        const transcription = transcribeResult.data.transcription;
+        // console.log('Transcription response: ', transcription);
+        
+        const sentimentAnalysis = await axios.post('/sentiment', { transcription: transcription });
+        const wordcloud = await axios.post('/wordcloud', { transcription:transcription });
 
-        const data = response.data;
-        console.log(data);
+        const data = {
+            transcribe: transcription,
+            sentiment: sentimentAnalysis.data.sentiment_analysis,
+            wordcloud: wordcloud.data.wordcloud
+        };
+
         return res.status(200).json({
-            message: "Transcribe Succeed",
+            message: "Succeed",
             data: data
         });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        return res.status(500).json({ message: "Error occurred", error: error.message });
+    }
+};
+
+const deleteTempFile = async (bucket, newFileName, res) => {
+    try {
+        await bucket.file(newFileName).delete();
+        console.log(`File with name ${newFileName} is deleted.`);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Error occurred", error: error.message });
     }
 }
 
+const process = async (transcribeResult) => {
+    const result = transcribeResult.data.transcription
+
+    console.log('Transcription response: ', result);
+
+    const sentimentAnalysis = await axios.post('/sentiment', {
+        transcription: result
+    })
+
+    const wordcloud = await axios.post('/wordcloud', {
+        transcription: result
+    })
+
+    const data = {
+        transcribe: transcribeResult.data.transcription,
+        sentiment: sentimentAnalysis.data.sentiment_analysis,
+        wordcloud: wordcloud.data.wordcloud
+    }
+
+    return data
+}
+
+const file = async(req, res, bucketName, bucket) => {
+    const file = req.file
+
+    const timestamp = Date.now();
+    const randomNum = Math.floor(Math.random() * 10000);
+    const fileExtension = path.extname(req.file.originalname);
+    const newFileName = `audio-${timestamp}-${randomNum}${fileExtension}`;
+
+    console.log("File Uploaded: ", file);
+
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(file.buffer);
+
+    const fileURL = `https://storage.googleapis.com/${bucketName}/${newFileName}`;
+
+    try {
+        const file = bucket.file(newFileName);
+        const streamPromise = new Promise((resolve, reject) => {
+            bufferStream.pipe(file.createWriteStream({
+                resumable: false,
+                metadata: {
+                    contentType: file.mimetype,
+                },
+            }))
+                .on('finish', resolve)
+                .on('error', reject);
+        });
+
+        await streamPromise;
+        console.log(`File uploaded to ${fileURL}`);
+
+        try {
+            const transcribeResult = await axios.post('/transcribe', {
+                url: fileURL,
+            });
+
+            deleteTempFile(bucket, newFileName, res);
+            const data = await process(transcribeResult);
+
+            return res.status(200).json({
+                message: 'File and form data uploaded and processed.',
+                data: data
+            }
+            );
+        } catch (transcriptionError) {
+            console.error('Error in transcription request:', transcriptionError);
+            return res.status(500).send('Error processing transcription.');
+        }
+    } catch (uploadError) {
+        console.error('Error uploading file to Google Cloud Storage:', uploadError);
+        return res.status(500).send('Error uploading file.');
+    }
+}
+
 module.exports = {
-    uploadFileHandler,
-    sentimentAnalysisHandler,
-    uploadLinkHandler
+    uploadHandler,
 }
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser")
 const axios = require('axios');
 
 const { bucketName, bucket, upload } = require('./bucket');
-const { uploadFileHandler, sentimentAnalysisHandler, uploadLinkHandler } = require('./handler');
+const { uploadHandler } = require('./handler');
 const { addUserHandler, addDataToFirestore } = require('./firestore');
 
 const app = express();
@@ -19,25 +19,9 @@ app.post('/api/adduser', (req, res) => {
     addUserHandler(req, res)
 })
 
-app.post('/api/uploadfile', upload.single('file'), async (req, res) => {
-    uploadFileHandler(req, res, bucket, bucketName)
+app.post('/api/upload', upload.single('file'), async (req, res) => {
+    uploadHandler(req, res, bucket, bucketName)
 });
-
-app.post('/api/uploadlink', (req, res) => {
-    // uploadLinkHandler(req, res)
-    const data = {
-        transcribe: "data.transcribe",
-        sentiment_analysis: "data.sentiment_analysis",
-        ner: "data.ner",
-        wordcloud: "data.wordcloud"
-    }
-
-    addDataToFirestore(data, "dummyUser", res);
-});
-
-app.post('/api/sentimentanalysis', async (req, res) => {
-    sentimentAnalysisHandler(req, res)
-})
 
 app.listen(port, () => {
     console.log("Server started on http://localhost:" + port);
