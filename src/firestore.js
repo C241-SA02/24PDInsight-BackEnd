ba62cde0dc2a0c539cbdc21150e155f2444aaec6
hanifsetyadi <hfsetyadi21@gmail.com>
2024-06-14 09:36:50 +0700

    new

--- src/firestore.js
+++ src/firestore.js
@@ -73,19 +73,6 @@ const updateDataToFirestore = async (
   await userDocRef.update(retreivedData);
 }
 
-// function getReadableTimestamp() {
-//   const date = new Date();
-//   const year = date.getFullYear();
-//   const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero for single-digit months
-//   const day = String(date.getDate()).padStart(2, '0');
-//   const hours = String(date.getHours()).padStart(2, '0');
-//   const minutes = String(date.getMinutes()).padStart(2, '0');
-//   const seconds = String(date.getSeconds()).padStart(2, '0');
-
-//   const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-//   return timestamp;
-// }
-
 module.exports = {
   addUserHandler,
   addDataToFirestore,
